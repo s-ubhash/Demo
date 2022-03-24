ec3d86c64a6eaa3ce384de626e950ea0f7761bc2
Subhash <csubhashse8@gmail.com>
2022-03-25 00:29:21 +0530

    Update demo.groovy

--- demo.groovy
+++ demo.groovy
@@ -13,12 +13,19 @@ pipeline {
               dir('f') {
                   sh 'find . -name "*.l"'
                   script{
-                    def files = findFiles(glob: "**/*.l") 
+                    def files = findFiles(glob: "**/*.yaml") 
                     echo """${files[0].name} ${files[0].path} ${files[0].directory} ${files[0].length} ${files[0].lastModified}""" 
                     for (int i = 0; i < files.size(); i++) {
                       def filename = files[i]
                       echo "${filename}"
-                     }
+                      data = readYaml file: "${filename}" 
+                      scan_path = data[scan_path]
+                        scan_path.each { e ->
+                echo "Translating ${e.getAt('application')} application 
+${e.getAt('path')}"
+} 
+                    }
+                      
                   }
               }
             }
