0cc38a034702e9ace505585bca52a023b1cd5a6b
Subhash <csubhashse8@gmail.com>
2022-03-25 00:15:27 +0530

    Update demo.groovy

--- demo.groovy
+++ demo.groovy
@@ -12,8 +12,10 @@ pipeline {
             steps {
               dir('f') {
                   sh 'find . -name "*.l"'
-                  def files = findFiles(glob: "**/*.l") 
-                  echo """${files[0].name} ${files[0].path} ${files[0].directory} ${files[0].length} ${files[0].lastModified}""" 
+                  script{
+                    def files = findFiles(glob: "**/*.l") 
+                    echo """${files[0].name} ${files[0].path} ${files[0].directory} ${files[0].length} ${files[0].lastModified}""" 
+                  }
               }
             }
         }
