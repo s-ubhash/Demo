064d51f66b65d1200e353d1fba9eb7b468f06b1f
Subhash <csubhashse8@gmail.com>
2022-03-24 21:30:51 +0530

    Update demo.groovy

--- demo.groovy
+++ demo.groovy
@@ -12,7 +12,7 @@ pipeline {
         stage('test') {
             steps {
               dir('f') {
-                  sh'ls -R ../*.l'
+                  sh'ls -R *.l'
               }
             }
         }
