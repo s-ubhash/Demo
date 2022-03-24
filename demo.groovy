d41e54fae74f064618da1cba14416ec2472f1313
Subhash <csubhashse8@gmail.com>
2022-03-25 00:30:32 +0530

    Update demo.groovy

--- demo.groovy
+++ demo.groovy
@@ -21,8 +21,7 @@ pipeline {
                       data = readYaml file: "${filename}" 
                       scan_path = data[scan_path]
                         scan_path.each { e ->
-                echo "Translating ${e.getAt('application')} application 
-${e.getAt('path')}"
+                echo "Translating ${e.getAt('application')} application ${e.getAt('path')}"
 } 
                     }
                       
