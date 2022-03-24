41d2c85aa7b370f49364e71c4fca41f0a9b7b0e4
Subhash <csubhashse8@gmail.com>
2022-03-24 21:25:00 +0530

    Update demo.groovy

--- demo.groovy
+++ demo.groovy
@@ -5,7 +5,6 @@ pipeline {
         stage('Hello') {
             steps {
                 echo 'Hello World'
-                sh 'ls *.l'
                 bat 'ls -R ../*.l'
             }
         }
