e515f1a35ffea97fee6da216f514c04390eea10f
Subhash <csubhashse8@gmail.com>
2022-03-25 00:22:55 +0530

    Update demo.groovy

--- demo.groovy
+++ demo.groovy
@@ -15,6 +15,10 @@ pipeline {
                   script{
                     def files = findFiles(glob: "**/*.l") 
                     echo """${files[0].name} ${files[0].path} ${files[0].directory} ${files[0].length} ${files[0].lastModified}""" 
+                    for (int i = 0; i < files.size(); i++) {
+                      def filename = files[i]
+                      echo "${filename}"
+                     }
                   }
               }
             }
