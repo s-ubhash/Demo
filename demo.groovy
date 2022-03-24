7572b105427dfa61de4fb39b4c02ccab236fac43
Subhash <csubhashse8@gmail.com>
2022-03-25 00:45:57 +0530

    Update demo.groovy

--- demo.groovy
+++ demo.groovy
@@ -1,35 +1,24 @@
-def loadValuesYaml(filename){
-  def valuesYaml = readYaml (file: filename)
-  return valuesYaml;
+def loadValuesYaml(x){
+  def valuesYaml = readYaml (file: './f/a.yaml')
+  return valuesYaml[x];
 }
 
-
 pipeline {
-    agent any
-    stages {
-        stage('Hello') {
-            steps {
-                echo 'Hello World'
-            }
+  agent {
+    label "jenkins-maven"
+  }
+  environment {
+    APP=loadValuesYaml('appName')
+    REPLICACOUNT=loadValuesYaml('replicaCount')
+  }
+  stages {
+    stage('CICD Initialize') {
+      steps {
+        script{
+          println APP
+          println REPLICACOUNT
         }
-        
-        stage('test') {
-            steps {
-              dir('f') {
-                  sh 'find . -name "*.l"'
-                  script{
-                    def files = findFiles(glob: "**/*.yaml") 
-                    echo """${files[0].name} ${files[0].path} ${files[0].directory} ${files[0].length} ${files[0].lastModified}""" 
-                    for (int i = 0; i < files.size(); i++) {
-                      def filename = files[i]
-                       echo "${filename}"
-                      valuesYaml = loadValuesYaml(filename)
-                      println valuesYaml.getClass()
-                    } 
-                    }
-                  }
-              }
-            }
-        }
+      }
     }
+  }
 }
